659d1cc7dca4af6a35bc347f37264646e513ff9e
Faiz <fazzfaiz123@gmail.com>
2023-12-27 10:16:02 +0530

    latest commit with no file structured

--- lib/Algorithms/Recursion/recursive_factorial.dart
+++ lib/Algorithms/Recursion/recursive_factorial.dart
@@ -1,11 +1,11 @@
 void main() {
-  int result = factorial(5);
+  int result = factorial(3);
   print(result);
 }
 
 factorial(n) {
   if (n <= 1) {
-    return 1;
+    return n;
   }
   return n * factorial(n - 1);
 }
