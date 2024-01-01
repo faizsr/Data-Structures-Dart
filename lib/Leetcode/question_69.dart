1cde3de51cd62030a667c7b8f82134b78dbbbb54
Faiz <fazzfaiz123@gmail.com>
2024-01-01 17:31:00 +0530

    My square function

---
+++ lib/Leetcode/question_69.dart
@@ -0,0 +1,16 @@
+int mySqrt(int x) {
+  int count = 0;
+  for (int i = 1; x > 0; i += 2) {
+    x -= i;
+    count++;
+  }
+  if (x != 0) {
+    return count - 1;
+  }
+  return count;
+}
+
+// void main() {
+//   int result = mySqrt(4);
+//   print(result);
+// }
