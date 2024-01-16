53e66fc2720643e7553da3dbe83f23610c9c1159
Faiz <fazzfaiz123@gmail.com>
2024-01-16 09:40:46 +0530

    Leetcode question 1796 answer corrected

--- lib/Leetcode/question_1796.dart
+++ lib/Leetcode/question_1796.dart
@@ -1,21 +1,15 @@
 int secondHighest(String s) {
   String newNum = s.replaceAll(RegExp(r'[^0-9]'), '');
-  List<int> num = [];
+  Set<int> num = {};
   for (int i = 0; i < newNum.length; i++) {
     num.add(int.parse(newNum[i]));
   }
-  num.sort();
-  int largest = -1;
-  for (int i = 0; i < num.length - 1; i++) {
-    if (num[i] < num[i + 1]) {
-      largest = num[i + 1];
-      break;
-    }
-  }
-  return largest;
+  List<int> numsList = num.toList();
+  numsList.sort();
+  return numsList.length <= 1 ? -1 : numsList[numsList.length - 2];
 }
 
 void main() {
-  int result = secondHighest('abc1111');
+  int result = secondHighest('abc114361');
   print(result);
 }
